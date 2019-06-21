3f1adf87a9d945225a3ad4b20daadff80f912aaf
eduardoferreira20 <eduardoferreira@polijuniorengenharia.com.br>
2019-06-21 18:19:47 -0300

    carrosel index

--- index.php
+++ index.php
@@ -217,63 +217,6 @@ background-attachment: fixed; filter:brightness(100%);">
 </div>
 </div>
 
-<div id="myCarousel" class="carousel slide" data-ride="carousel">
-  <!-- Indicators -->
-  <ul class="carousel-indicators">
-    <li data-target="#myCarousel" data-slide-to="0" class="active"></li>
-    <li data-target="#myCarousel" data-slide-to="1"></li>
-    <li data-target="#myCarousel" data-slide-to="2"></li>
-  </ul>
-  <ul class="caror">
-      <li>a</li>
-  </ul>
-
-  <!-- Wrapper for slides -->
-  <div class="carousel-inner">
-    <div class="item active">
-      <img src="la.jpg" alt="Los Angeles">
-    </div>
-
-    <div class="item">
-      <img src="chicago.jpg" alt="Chicago">
-    </div>
-
-    <div class="item">
-      <img src="ny.jpg" alt="New York">
-    </div>
-  </div>
-
-  <!-- Left and right controls -->
-  <a class="left carousel-control" href="#myCarousel" data-slide="prev">
-    <span class="glyphicon glyphicon-chevron-left"></span>
-    <span class="sr-only">Previous</span>
-  </a>
-  <a class="right carousel-control" href="#myCarousel" data-slide="next">
-    <span class="glyphicon glyphicon-chevron-right"></span>
-    <span class="sr-only">Next</span>
-  </a>
-</div>
-
-<div class="carousel-control">
-     <ul>
-        <li>1</li>
-        <li>2</li>
-        <li>3</li>
-        <li>4</li>
-     </ul>
-     <ul>
-        <li>1</li>
-        <li>2</li>
-        <li>3</li>
-        <li>4</li>
-        <li>5</li>
-        <li>6</li>
-     </ul>
-     <ul>
-        <li>1</li>
-        <li>2</li>
-     </ul>
-</div>
 
 <div class="container">
     <div class="row">
